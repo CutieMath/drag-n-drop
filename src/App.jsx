8e010bfad84b77677e672fc247718497d571c641
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-26 13:50:25 +1100

    create base for drag and drop

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="dnd-container">
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>Item 1</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 2</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 3</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 4</p>
+            </div>
+          </div>
+        </div>
+        <div className="dnd-group">
+          <div className="dnd-item">
+            <div>
+              <p>Item 1</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 2</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 3</p>
+            </div>
+          </div>
+          <div className="dnd-item">
+            <div>
+              <p>Item 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
