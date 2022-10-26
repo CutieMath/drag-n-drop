1c7b283077f1bd6aa577591f7820c770308e751d
Yuxin Ye <yuxin_ye@protonmail.com>
2022-10-26 14:44:29 +1100

    fix radius while drag. Add draggable events

--- src/components/DragNDrop.jsx
+++ src/components/DragNDrop.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 
 const DragNDrop = ({ data }) => {
   const [list, setList] = useState(data);
+
+  const handleDragStart = (e, index) => {
+    console.log("dragstart:", e);
+  };
+
   return (
     <div className="dnd-container">
       {list.map((group, index) => (
         <div className="dnd-group" key={index}>
           <div className="group-title">{group.title}</div>
           {group.items.map((item, itemIndex) => (
-            <div draggable className="dnd-item" key={itemIndex}>
+            <div
+              draggable
+              onDragStart={handleDragStart}
+              className="dnd-item"
+              key={itemIndex}
+            >
               {item}
             </div>
           ))}
